18b626d6c4b3ed95466c05c1197768d8ef16c276
Murillo Mendonça <mmendoncamascarenha@gmail.com>
2025-03-02 23:36:06 -0300

    ultimas atualizaçoes

---
+++ aula03.js
@@ -0,0 +1,23 @@
+"use strict"
+
+
+
+if(true){
+    let nome="Murillo"//Tem escopo de bloco, ou seja, só pode ser acessada dentro do bloco onde foi declarada ({}).
+}
+
+console.log(nome) 
+
+/*
+function exemploLet() {
+    if (true) {
+      let a = 20;
+      console.log(a); // 20
+    }
+    // console.log(a); // Erro! "a" não existe fora do bloco
+  }
+  
+  console.log(b); // Erro! Não pode acessar antes da declaração
+  let b = 10;
+  console.log(b); // 10
+*/
